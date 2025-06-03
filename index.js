3932a77f4cc0149f1bcf5557d12ee3ebe94b07ec
Divi22005 <23r11a66b8@gcet.edu.in>
2025-06-03 14:45:06 +0530

    updated

--- index.js
+++ index.js
@@ -1,51 +1,19 @@
-import express from 'express';
-import cors from 'cors';
-import mongoose from 'mongoose';
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+
+import userRouter from "./routes/userRoutes.js";
+import productRouter from "./routes/productRoutes.js";
 
 const app = express();
-const PORT = 8080;
-
 app.use(cors());
 app.use(express.json());
 
-const userSchema = new mongoose.Schema({
-  name: String,
-});
-const User = mongoose.model('User', userSchema);
+app.use("/users", userRouter);
 
-const productList = [
-  { id: 1, name: 'Laptop', price: 1000 },
-  { id: 2, name: 'Smartphone', price: 500 },
-  { id: 3, name: 'Tablet', price: 300 }
-];
+app.use("/products", productRouter);
 
-app.get('/products', (req, res) => {
-  res.json(productList);
-});
-
-app.listen(PORT, () => {
-  mongoose.connect('mongodb://localhost:27017/gcet');
-  console.log(`Server running at http://localhost:${PORT}`);
-});
-
-app.get('/', (req, res) => {
-  res.send('Hello Vignesh, Welcome to the world of Node.js');
-}); 
-
-app.get('/greet', (req, res) => {
-  res.send('Greetings from the server!');
-});
-
-app.get('/name', (req, res) => {
-  res.send('Vignesh Kumar');
-});
-
-app.get('/weather', (req, res) => {
-  res.json({ temp: "31°C", condition: "Sunny" });
-});
-
-app.post('/register', async (req, res) => {
-  const { name } = req.query;
-  const result = await User.insertOne({ name: 'jhon' });
-  res.json(result);
+app.listen(8080, () => {
+  mongoose.connect("mongodb://localhost:27017/gcet");
+  console.log("Server Started");
 });
